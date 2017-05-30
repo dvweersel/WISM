f3be3c7c6148fdf39e3606259356683fad9156af
kilianbakker <k.j.bakker@students.uu.nl>
2017-05-30 13:29:39 +0200

    Add files via upload

--- Template/AnalysisRobot.m
+++ Template/AnalysisRobot.m
@@ -13,13 +13,26 @@ classdef AnalysisRobot < AutoTrader
             
             %% Store the depth of the stocks
             aBot.depth.(aTime) = struct(aDepth);
-            aBot.depth.(aTime).stockPrice = StockPrice(aBot.depth.(aTime).askLimitPrice, aBot.depth.(aTime).bidLimitPrice);
-            
+            aBot.depth.(aTime).stockPrice = StockPrice(aBot.depth.(aTime).askLimitPrice, aBot.depth.(aTime).bidLimitPrice)
+                        
             %% Store the depth of the options
             nOption = GetAllOptionISINs();
 
             [S, T, p, K] = ParseOptionISINs(nOption);
-            aBot.optionDepth.(aTime) = struct('T', T, 'p', p, 'K', K);
+            
+            N=length(p);
+            for v = 1:N
+                if p(v) == 'Call'
+                    p(v) = 0;
+                else
+                    p(v) = 1;
+                end
+            end
+            V=0;
+            
+            
+            
+            aBot.optionDepth.(aTime) = struct('T', T, 'p', p, 'K', K,'V',V);
         end
         
         function ShowPlots(aBot)
@@ -27,10 +40,19 @@ classdef AnalysisRobot < AutoTrader
             PlotStock(aBot);
 
             %% 3
-            PlotOptionSpread(aBot)
+            PlotOptionSpread(aBot);
             
             %% 4
-            PlotOptionTime(aBot)
+            PlotOptionTime(aBot);
+            
+            %% 5
+            PlotOptionIV1(aBot);
+            
+            %% 6
+            PlotOptionIV2(aBot);
+            
+            %% 7 
+            PlotGreeks(aBot);
         end
         
         function PlotStock(aBot)
@@ -83,9 +105,112 @@ classdef AnalysisRobot < AutoTrader
         end
         
         function PlotOptionTime(aBot)
-          
+            
+        end
+        
+        function PlotOptionIV1(aBot)
+            plotTime = 0;
+            N=length(aBot.optionDepth.(aTime).V);
+            optionIV1 =  zeros(N,1);
+            for v = 1:N
+                optionIV1(v)=IV(aBot.depth.(plotTime).stockPrice, ...
+                                aBot.optionDepth.(plotTime).K(v),...
+                                aBot.optionDepth.(plotTime).T(v),...
+                                aBot.optionDepth.(plotTime).V(v),...
+                                aBot.optionDepth.(plotTime).p(v));
+            end
+            plot(aBot,optionDepth.(plotTime).K,optionIV1)
+        end
+                
+        function PlotOptionIV2(aBot)
+            nTime = fieldnames(aBot.depth);
+            lastTimeString = curly(nTime(end),1);
+            lastTime = str2double(lastTimeString(2:end));
+ 
+            optionIV2 = zeros(lastTime,1);
+ 
+            for t= 1:lastTime
+                N=length(aBot.optionDepth.(t).K);
+                optionIV2(t) = zeros(1,N);
+                for k=1:N
+                    optionIV2(t,k) = IV(aBot.depth.(t).stockPrice, ...
+                                        aBot.optionDepth.(t).K(k),...
+                                        aBot.optionDepth.(t).T(k),...
+                                        aBot.optionDepth.(t).V(k),...
+                                        aBot.optionDepth.(t).p(k));                    
+                end
+            end
+            
+            for k=1:length(aBot.optionDepth.(0).K)
+               t=1:lastTime;
+               plot(optionIV2(t,k),t)
+               hold on
+            end
+            hold off
+        end
+        
+        function PlotGreeks(aBot)
+            nTime = fieldnames(aBot.depth);
+            lastTimeString = curly(nTime(end),1);
+            lastTime = str2double(lastTimeString(2:end));
+ 
+            optionIV2 = zeros(lastTime,1);
+ 
+            for t= 1:lastTime
+                N=length(aBot.optionDepth.(t).K);
+                optionIV2(t) = zeros(1,N);
+                for k=1:N
+                    optionIV2(t,k) = IV(aBot.depth.(t).stockPrice, ...
+                                        aBot.optionDepth.(t).K(k),...
+                                        aBot.optionDepth.(t).T(k),...
+                                        aBot.optionDepth.(t).(k),...
+                                        aBot.optionDepth.(t).p(k));                    
+                end
+            end
+            
+            optionDeltas = zeros(lastTime,1);
+            optionGammas = zeros(lastTime,1);
+            optionVegas = zeros(lastTime,1);
+            for t= 1:lastTime
+                N=length(aBot.optionDepth.(t).K);
+                optionDeltas(t) = zeros(1,N);
+                optionGammas(t) = zeros(1,N);
+                optionVegas(t) = zeros(1,N);
+                for k=1:N
+                    BSM = BS(aBot.depth.(t).stockPrice, ...
+                            aBot.optionDepth.(t).K(k),...
+                            aBot.optionDepth.(t).T(k),...
+                            optionIV2(t,k),...
+                            aBot.optionDepth.(t).p(k));   
+                    optionDeltas(t,k)=BSM(2,1);
+                    optionGammas(t,k)=BSM(3,1);
+                    optionVegas(t,k)=BSM(4,1);
+                end
+            end
+            
+            for k=1:length(aBot.optionDepth.(0).K)
+               t=1:lastTime;
+               subplot(3,1,1)
+               plot(optionDeltas(t,k),t)
+               hold on
+            end
+            hold off
+            
+            for k=1:length(aBot.optionDepth.(0).K)
+               t=1:lastTime;
+               subplot(3,1,2)
+               plot(optionGammas(t,k),t)
+               hold on
+            end
+            hold off
+            
+            for k=1:length(aBot.optionDepth.(0).K)
+               t=1:lastTime;
+               subplot(3,1,3)
+               plot(optionVegas(t,k),t)
+               hold on
+            end
+            hold off
         end
     end
 end
-
-
