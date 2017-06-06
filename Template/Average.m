0d058bb7b5e008b0ba59f8dadf7cb58dd5f4da4d
kilianbakker <k.j.bakker@students.uu.nl>
2017-06-06 22:00:00 +0200

    rename stockprice to average

---
+++ Template/Average.m
@@ -0,0 +1,15 @@
+function A = Average(aAskPrices, aBidPrices)
+    %   Returns the average price    
+
+    SKEW = 0;
+    
+    if(~isempty(aAskPrices) && ~isempty(aBidPrices))
+        A = ((1-SKEW)*aAskPrices(1) + (1+SKEW)*aBidPrices(1))/2;
+    elseif(isempty(aAskPrices) && ~isempty(aBidPrices))
+        A = aBidPrices(1);
+    elseif(~isempty(aAskPrices) && isempty(aBidPrices))
+        A = aAskPrices(1);
+    else
+        A = NaN;
+    end
+end
