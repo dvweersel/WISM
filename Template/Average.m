3538e93ac88048d439a32fbcfd97621d1841649d
kilianbakker <k.j.bakker@students.uu.nl>
2017-06-11 18:07:27 +0200

    Average updated again

--- Template/Average.m
+++ Template/Average.m
@@ -1,17 +1,13 @@
 function A = Average(aAskPrices, aBidPrices)
-    %   Returns the average price    
+    %   Returns the stock price    
 
     SKEW = 0;
     
-    if(~isempty(aAskPrices) && ~isempty(aBidPrices) && ~isnan(aBidPrices(1)) && ~isnan(aAskPrices(1)))
+    if(~isempty(aAskPrices) && ~isempty(aBidPrices))
         A = ((1-SKEW)*aAskPrices(1) + (1+SKEW)*aBidPrices(1))/2;
-    elseif(isempty(aAskPrices) && ~isempty(aBidPrices) && ~isnan(aBidPrices(1)))
+    elseif(isempty(aAskPrices) && ~isempty(aBidPrices))
         A = aBidPrices(1);
-    elseif(isnan(aAskPrices) && ~isempty(aBidPrices) && ~isnan(aBidPrices(1)))
-        A = aBidPrices(1);
-    elseif(~isempty(aAskPrices) && isempty(aBidPrices) && ~isnan(aAskPrices(1)))
-        A = aAskPrices(1);
-    elseif(~isempty(aAskPrices) && isnan(aBidPrices) && ~isnan(aAskPrices(1)))
+    elseif(~isempty(aAskPrices) && isempty(aBidPrices))
         A = aAskPrices(1);
     else
         A = NaN;
