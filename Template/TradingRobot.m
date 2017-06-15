1d4e5e00f523a8cca72ea3469ce1957a74d19e42
kilianbakker <k.j.bakker@students.uu.nl>
2017-06-15 16:46:40 +0200

    update tradingrobot

--- Template/TradingRobot.m
+++ Template/TradingRobot.m
@@ -3,15 +3,12 @@ classdef TradingRobot < AutoTrader
         depth = struct
         optionISIN = struct
         time = 0
-<<<<<<< HEAD
         
         % The position at the ING
         myINGPosition = zeros(1,1)
         
         % The positions at the ING options
         myINGOptionPositions = zeros(1,20)
-=======
->>>>>>> b752e4b11c820225754613a4acd8f5f9d3e842d8
     end
 
     methods
@@ -49,7 +46,6 @@ classdef TradingRobot < AutoTrader
                 CalculateIV(aBot,ISIN);
                 CalculateGreeks(aBot,ISIN);
             end
-<<<<<<< HEAD
             
             %% look for arbitrage opportunities in the options
             k=0;
@@ -116,8 +112,6 @@ classdef TradingRobot < AutoTrader
             
             aBot.depth.(aTime).ING.stockPrice = ...
                 Average(askLimitPrice, bidLimitPrice);
-=======
->>>>>>> b752e4b11c820225754613a4acd8f5f9d3e842d8
         end
         
         %%  Calculates the implied volatility by averaging the value found
@@ -247,95 +241,5 @@ classdef TradingRobot < AutoTrader
         function Unwind(aBot)
             
         end
-        
-                %% Calculates the stock price
-        function CalculateStockPrice(aBot)
-            aTime = strcat('t', num2str(aBot.time));
-            
-            if(isempty(aBot.depth.(aTime).ING.askLimitPrice))
-                askLimitPrice = [];
-            else
-                askLimitPrice = aBot.depth.(aTime).ING.askLimitPrice;
-            end
-            
-            if(isempty(aBot.depth.(aTime).ING.bidLimitPrice))
-                bidLimitPrice = [];
-            else
-                bidLimitPrice = aBot.depth.(aTime).ING.bidLimitPrice;
-            end
-            
-            aBot.depth.(aTime).ING.stockPrice = ...
-                Average(askLimitPrice, bidLimitPrice);
-        end
-        
-        %%  Calculates the implied volatility by averaging the value found
-        %   using the bid and ask
-        function CalculateIV(aBot, ISIN)
-            aTime = strcat('t', num2str(aBot.time));
-
-            % The position of ISIN in optionISIN
-            for i=1:length(aBot.optionISIN.ISIN)
-                cell = aBot.optionISIN.ISIN(i);
-                if(strcmp(cell{1}, ISIN))
-                    n = i;
-                    break;
-                end
-            end
-            
-            % The stockprice needs to be defined to calculate IV
-            if isfield(aBot.depth.(aTime).ING, 'stockPrice')
-                if ~isempty(aBot.depth.(aTime).(ISIN).askLimitPrice)
-                    optionAskIV = IV(aBot.depth.(aTime).ING.stockPrice, ...
-                        aBot.optionISIN.K(n),...
-                        aBot.optionISIN.T(n),...
-                        aBot.depth.(aTime).(ISIN).askLimitPrice(1),...
-                        aBot.optionISIN.p(n));
-                else
-                    optionAskIV = [];
-                end
-                
-                if ~isempty(aBot.depth.(aTime).(ISIN).bidLimitPrice)
-                    optionBidIV = IV(aBot.depth.(aTime).ING.stockPrice, ...
-                        aBot.optionISIN.K(n),...
-                        aBot.optionISIN.T(n),...
-                        aBot.depth.(aTime).(ISIN).bidLimitPrice(1),...
-                        aBot.optionISIN.p(n));
-                else
-                    optionBidIV = [];
-                end
-                
-                aBot.depth.(aTime).(ISIN).IV = Average(optionAskIV,optionBidIV);
-            end
-        end
-        
-        %% Calculates the Greeks
-        function CalculateGreeks(aBot,ISIN)
-            aTime = strcat('t', num2str(aBot.time));
-            
-            % The position of ISIN in optionISIN
-            for i=1:length(aBot.optionISIN.ISIN)
-                cell = aBot.optionISIN.ISIN(i);
-                if(strcmp(cell{1}, ISIN))
-                    n = i;
-                    break;
-                end
-            end
-            
-            % The stockprice needs to be defined to calculate IV
-            if isfield(aBot.depth.(aTime).ING, 'stockPrice')
-                BSM = BS(aBot.depth.(aTime).ING.stockPrice, ...
-                        aBot.optionISIN.K(n),...
-                        aBot.optionISIN.T(n),...
-                        aBot.depth.(aTime).(ISIN).IV,...
-                        aBot.optionISIN.p(n));
-                    aBot.depth.(aTime).(ISIN).Delta = BSM(2,1);
-                    aBot.depth.(aTime).(ISIN).Gamma = BSM(3,1);
-                    aBot.depth.(aTime).(ISIN).Vega  = BSM(4,1);
-            end
-        end
-        
-        function varargout = curly(x, varargin)
-            [varargout{1:nargout}] = x{varargin{:}};
-        end    
     end
 end
